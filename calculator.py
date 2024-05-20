f955b950e102717d3597fe6083fc270c70d0b8d2
nasimp <parvez15-6422@s.diu.edu.bd>
2024-05-20 15:11:38 +0600

    Fix Done

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ class Calculator:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("375x667")
-        self.window.resizable(0, 0)
+        self.window.resizable(False, False)  # Fixed: Replace (0, 0) with (False, False)
         self.window.title("Calculator")
 
         self.total_expression = ""
